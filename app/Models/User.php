cccbe4b7a5eeaf6527ca82e577ebc1021dcf6753
azizah <azizahkeylanova7@gmail.com>
2026-08-19 14:09:51 +0700

    p

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasFactory;
 
     // 1. Beritahu nama tabel kustom kamu
     protected $table = 'user'; 
